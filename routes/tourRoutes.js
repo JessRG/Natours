b7f02f6c755d81d236dbad7159986504cb862d46
Jesus Garza <jesus.r.g27@gmail.com>
2019-08-25 15:02:00 -0500

    Added in a model directory to implement the Model View Controller (MVC) Architecture. Specified the Schema and built the Tour model within the model directory. Modified the tour controllers to implement async/awaitwith try/catch blocks. Added in mongoose drivers and connected to the mongoose atlas cluster which can then be monitored easily with the MongoDB Compass tool/UI. Added import script within the dev-data/data directory to create database (DB) documents from the tours-simple.json file and delete documents/collection from the DB.

--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -1,17 +1,12 @@
 const express = require('express');
 const tourController = require('./../controllers/tourController');
-const router = express.Router();
 
-router.param('id', tourController.checkID);
-// Create a checkBody middleware
-// Check if body contains the name and price property
-// If not, send back 400 (bad request)
-// Add it to the post handler stack
+const router = express.Router();
 
 router
   .route('/')
   .get(tourController.getAllTours)
-  .post(tourController.checkBody, tourController.createTour);
+  .post(tourController.createTour);
 
 router
   .route('/:id')
