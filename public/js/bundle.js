e2189f300036e2fa84db0918b354812e73e6bdf3
Jesus Garza <jesus.r.g27@gmail.com>
2019-10-30 01:27:04 -0500

    Installed Multer package to handle multi-part form data.
    Used Multer to allow a user to upload photos to the /updateMe route, so users will be able to update their name, email, and also update/upload their user photos. Implemented disk storage and file filter for Multer.
    
    Added an upload and resize function/middleware to format/crop user photos into squares. Installed the Sharp package which is a high speed Node.js module that can convert large images in common formats to smaller, web-friendly JPEG, PNG and WebP images of varying dimensions.
    
    Added a new FormData, a structure that provides a way to easily construct a set of key/value pairs representing form fields and their values. It uses the same format a form would use if the encoding type were set to ‘multipart/form-data’.
    
    Added an upload and resize function for tour images which uploads both the currently selected tour cover image and a few additional images. Multiple tour images are handled within the tour controller.
    
    Added a complex Email Handler class within email.js to handle sending emails depending on the node environment variable setting (production, development).
    
    Utilized SendGrid by making an account at the SendGrid website and defining the SendGrid service, my username, and my password within the nodemailer transport options. SendGrid service is utilized for the purpose of allowing the application/api to send emails to a user’s professional or personal email account when in production.

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -8861,13 +8861,14 @@ if (loginForm) loginForm.addEventListener('submit', function (e) {
 });
 if (logOutBtn) logOutBtn.addEventListener('click', _login.logout);
 if (userDataForm) userDataForm.addEventListener('submit', function (e) {
-  e.preventDefault();
-  var name = document.getElementById('name').value;
-  var email = document.getElementById('email').value;
-  (0, _updateSettings.updateSettings)({
-    name: name,
-    email: email
-  }, 'data');
+  e.preventDefault(); // uses the same format a form would use if the encoding type were set to "multipart/form-data"
+
+  var form = new FormData();
+  form.append('name', document.getElementById('name').value);
+  form.append('email', document.getElementById('email').value);
+  form.append('photo', document.getElementById('photo').files[0]);
+  console.log(form);
+  (0, _updateSettings.updateSettings)(form, 'data');
 });
 if (userPasswordForm) userPasswordForm.addEventListener('submit',
 /*#__PURE__*/
@@ -8938,7 +8939,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50056" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50212" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
