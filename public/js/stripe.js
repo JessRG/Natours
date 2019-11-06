2dc06232fc8ba15ee521ace57026b9e52ba6e560
Jesus Rogelio Garza <jesus.r.g27@gmail.com>
2019-11-06 15:41:27 -0600

    Prepared app for deployment

--- public/js/stripe.js
+++ public/js/stripe.js
@@ -6,10 +6,8 @@ const stripe = Stripe('pk_test_cOlqWQa0HxRJ5HP0tKAjWGCJ00pb9mnA6v');
 const bookTour = async tourId => {
   try {
     // 1) Get checkout session from endpoint from API
-    const session = await axios(
-      `http://127.0.0.1:8000/api/v1/bookings/checkout-session/${tourId}`
-    );
-    console.log(session);
+    const session = await axios(`/api/v1/bookings/checkout-session/${tourId}`);
+    // console.log(session);
 
     // 2) Create checkout form + charge credit card
     await stripe.redirectToCheckout({
